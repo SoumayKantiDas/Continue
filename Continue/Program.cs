d7679ce310ebcd1d4f633fff0972d9eabaaa1588
Soumay kanti Das <anikds2859@outlook.com>
2021-06-02 15:19:27 +0600

    Add project files.

---
+++ Continue/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i =1; i<= 10; i++)
+            {
+                if (i < 9)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
